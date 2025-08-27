1490a8c1fbb9db0044526c88151f5137abd029fd
Samiksha-Mishra <159643240+Samiksha-Mishra@users.noreply.github.com>
2025-08-27 23:02:34 +0530

    app.py

--- app.py
+++ app.py
@@ -419,5 +419,4 @@ def talktrace_predict():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(debug=True)
